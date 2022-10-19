281d8bf7d05af3ee9112b77e23a557dbde387974
ThisIsLexx <alexisguzman604@gmail.com>
2022-10-19 14:15:38 -0500

    Agregando un componente template para usar en todos los archivos, también se implemento el framework materialize.css

--- resources/views/usuarios/usuarioIndex.blade.php
+++ resources/views/usuarios/usuarioIndex.blade.php
@@ -5,24 +5,31 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mariscos cheos - Login</title>
+
+    @vite(['resources/css/materialize.css', 'resources/js/materialize.js'])
 </head>
+
 <body>
-    <h2>INICIO DE SESIÓN</h2>
+    <h2 class="center">MARISCOS CHEO'S</h2>
+    <h4 class="center">since 1989</h4>
 
-    <form action="/usuario_SignIn" method="POST">
-        @csrf
-
-        <label for="username">Nombre de usuario</label><br>
-        <input type="text" name="username"><br>
-
-        <label for="password">Contraseña</label><br>
-        <input type="password" name="password"><br><br>
-
-        <input type="submit" value="Iniciar sesión">
-    </form>
+    <div class="card">
+        <form action="/usuario_SignIn" method="POST">
+            @csrf
+    
+            <label for="username">Nombre de usuario</label><br>
+            <input type="text" name="username" required><br>
+    
+            <label for="password">Contraseña</label><br>
+            <input type="password" name="password" required><br><br>
+    
+            <input type="submit" value="Iniciar sesión" class="btn deep-orange lighten-2">
+                
+        </form>        
+    </div>
 
     <p>
-        No tienes una cuenta? <a href="/usuario/create">Registrate aqui!</a>
+        No tienes una cuenta? <a href="/usuario/create" class="deep-orange-text lighten-2">Registrate aqui!</a>
     </p>
 
 </body>
